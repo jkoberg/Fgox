a5236b1a8691ac98e72b9cc36a0500b2e75e8de8
Joe Koberg <joe.koberg@versionone.com>
2013-12-30 13:08:41 -0500

    Move the API multipliers into static memebrs so they can be referred to

--- Fgox/Api.fs
+++ Fgox/Api.fs
@@ -7,17 +7,18 @@ open Fgox.Config
 
 open Fgox.Types.Units
 
+
 type Api(secrets:Secrets) =
   let http = new Fgox.Http.Client(secrets)
-  let priceFactor = 10e5M</dollar> 
-  let qtyFactor = 1M</satoshi>
+  static member priceFactor = 10e5M</dollar> 
+  static member qtyFactor = 1M</satoshi>
 
   member api.Sell (qty:Btc) (price:Dollar) = async {
     let! resp, json =
       http.send "api/1/generic/private/order/add" [
         "type", "ask" 
-        "amount_int", (btcToSatoshi qty * qtyFactor |> round).ToString()
-        "price_int", (price * priceFactor |> round).ToString()
+        "amount_int", (btcToSatoshi qty * Api.qtyFactor |> round).ToString()
+        "price_int", (price * Api.priceFactor |> round).ToString()
         ]
     return json
     }
